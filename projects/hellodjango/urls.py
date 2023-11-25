f80c970641d591e76792b233d533cd5b8f287465
Abmccullough1 <wmccullough24@basecampcodingacademy.com>
2023-11-25 10:42:53 -0600

    done?

---
+++ projects/hellodjango/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import hello_view
+
+urlpatterns = [
+    path("hello/", hello_view),
+    path("admin/", admin.site.urls),
+]
